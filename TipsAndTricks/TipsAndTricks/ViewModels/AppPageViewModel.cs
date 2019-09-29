0c5d1b8565c1379709b2d9d8adc9449295af744f
Emilio Escaño <emiliojoseescano@hotmail.com>
2019-09-29 12:56:21 -0400

    Add project files.

---
+++ TipsAndTricks/TipsAndTricks/ViewModels/AppPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipsAndTricks.ViewModels
+{
+    
+    public class AppPageViewModel
+    {
+        public string Username { get; set; } = "Pablito";
+
+    }
+}
---
+++ TipsAndTricks/TipsAndTricks/Views/AppPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TipsAndTricks.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TipsAndTricks.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppPage : ContentPage
+    {
+        public AppPage()
+        {
+            InitializeComponent();
+            BindingContext = new AppPageViewModel();
+        }
+    }
+}
